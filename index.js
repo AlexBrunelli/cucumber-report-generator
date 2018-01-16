ad8e8d85bf6f839ef02f1bdd0ae1ce763e1f7b37
alexandre_brunelli <abrunelli@test-cloud.com>
2018-01-16 12:03:58 +0100

    Update: Remove metadata

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ const options = {
     output: REPORTPATH,
     reportSuiteAsScenarios: true,
     launchReport: true,
-    metadata: {
-        "App Version":"0.3.2",
-        "Test Environment": "STAGING",
-        "Browser": "Chrome  54.0.2840.98",
-        "Platform": "Windows 10",
-        "Parallel": "Scenarios",
-        "Executed": "Remote"
-    }
 };
 
 reporter.generate(options)
